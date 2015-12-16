54aa2071d4122773397ea3604b5e76d33bb74ad7
Alejandro <AlexMJHS@gmail.com>
2015-12-16 08:48:11 -0700

    Created Robot Project

---
+++ src/bot/controller/AlejandroBotRunner.java
@@ -0,0 +1,10 @@
+package bot.controller;
+
+public class AlejandroBotRunner
+{
+	public static void main(String[] args)
+	{
+		BotController myBotController = new BotController();
+		myBotController.start();
+	}
+}
